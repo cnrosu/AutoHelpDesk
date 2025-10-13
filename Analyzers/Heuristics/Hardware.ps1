43d87fd014ef6f2d71eb1d17c2c248875bbbc026
cnrosu <103119670+cnrosu@users.noreply.github.com>
2025-10-14 09:02:16 +1100

    Load hardware inventory functions

--- Analyzers/Heuristics/Hardware.ps1
+++ Analyzers/Heuristics/Hardware.ps1
@@ -6,6 +6,7 @@
 . (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath 'AnalyzerCommon.ps1')
 
 $hardwareModuleRoot = Join-Path -Path $PSScriptRoot -ChildPath 'Hardware'
+. (Join-Path -Path $hardwareModuleRoot -ChildPath 'Inventory.ps1')
 . (Join-Path -Path $hardwareModuleRoot -ChildPath 'Common.ps1')
 . (Join-Path -Path $hardwareModuleRoot -ChildPath 'Normalization.ps1')
 . (Join-Path -Path $hardwareModuleRoot -ChildPath 'Events.ps1')
